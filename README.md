e15a3a71698962fe24020d9cf44ff443ec8ab406
Eric Xu <ericxu343@gmail.com>
2020-03-30 21:39:38 -0400

    Fixing the structure and celaning the code for RobotSoccerSimulation

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Import this repository into a new repository of your own and write the code acco
 
 In your new repository, don’t forget to edit this README file, as _your_ repository will certainly not contain any unfinished Java programs.
 
-I have created a Soccer simulation with ball graphics that move given its assigned speed on its x-value and a y-value. There is the player ball that has its special variable assignments. Whether the ball successfully enters the goal depends on the friction and the speed of the other balls. If the player makes it to the goal, it is a goal, but if the player stops, then the game ends. 
+Using the JavaScript model as a helper, I added methods into the Soccer simulation with ball graphics that moved given its assigned speed on its x-value and a y-value so it would work such as checking if it was in the goal, applying friction, and applying constraints. There is the player ball that has its special variable assignments. Whether the ball successfully enters the goal depends on the friction and the speed of the other balls. If the player makes it to the goal, it is a goal, but if the player stops, then the game ends. 
--- RobotSoccerSimulation.java
+++ RobotSoccerSimulation.java
@@ -10,20 +10,15 @@ import javax.swing.SwingUtilities;
 public class RobotSoccerSimulation extends JPanel {
     private static final long serialVersionUID = -5228718339006830546L;
 
-    // The soccer field
     private static double WIDTH = 400;
     private static double HEIGHT = 600;
 
-    // Simulation parameters: in your lab, set these from command line arguments
     private static double PLAYER_RADIUS;
     private static double ENEMY_RADIUS;
     private static double PLAYER_SPEED;
     private static double ENEMY_SPEED;
     private static double FRICTION;
 
-    //
-
-    // Initially null; Set this to a string to end the simulation
     private volatile String endMessage;
 
     static class Ball {
@@ -34,7 +29,6 @@ public class RobotSoccerSimulation extends JPanel {
         private Color color;
 
         Ball(double x, double y, double radius, double speed, Color color) {
-            // You know what to do here :)
             this.x = x;
             this.y = y;
             this.radius = radius;
@@ -43,7 +37,6 @@ public class RobotSoccerSimulation extends JPanel {
         }
 
         void moveToward(double targetX, double targetY) {
-            // Fill this in
             var dx = targetX - this.x;
             var dy = targetY - this.y;
             var v = speed / Math.hypot(dx, dy);
@@ -51,18 +44,12 @@ public class RobotSoccerSimulation extends JPanel {
             this.y = constrain(this.y + v * dy, this.radius, HEIGHT - this.radius);
         }
 
-        // Slow down the ball by FRICTION. Don't let it go negative, though!
         void applyFriction() {
-            // Fill this in
             this.speed = constrain(this.speed - FRICTION, 0, Double.POSITIVE_INFINITY);
-
         }
 
-        // Returns whether the ball is *entirely* inside the goal
         boolean inside(Goal g) {
-          // check this one
-          return this.x - this.radius > g.x - g.w / 2 && this.x + this.radius < g.x + g.w / 2 && this.y - this.radius > g.y - g.h / 2 && this.y + this.radius < g.y + g.h / 2;
-             // <--------- FIX THIS
+            return this.x - this.radius > g.x - g.w / 2 && this.x + this.radius < g.x + g.w / 2 && this.y - this.radius > g.y - g.h / 2 && this.y + this.radius < g.y + g.h / 2;
         }
     }
 
@@ -75,15 +62,13 @@ public class RobotSoccerSimulation extends JPanel {
         double h = 100;
     }
 
-    private static Goal goal = new Goal(); // creates a goal
+    private static Goal goal = new Goal();
 
-    // You don't need to touch this one.
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (var ball : balls) {
             g.setColor(ball.color);
-            g.fillOval((int) (ball.x - ball.radius), (int) (ball.y - ball.radius), (int) ball.radius * 2,
-                    (int) ball.radius * 2);
+            g.fillOval((int) (ball.x - ball.radius), (int) (ball.y - ball.radius), (int) ball.radius * 2, (int) ball.radius * 2);
         }
         g.setColor(new Color(255, 255, 255, 128));
         g.fillRect((int) (goal.x - goal.w / 2), (int) (goal.y - goal.h / 2), (int) goal.w, (int) goal.h);
@@ -96,18 +81,10 @@ public class RobotSoccerSimulation extends JPanel {
 
     private void runTheAnimation() {
         while (endMessage == null) {
-            //
-            // TODO: Update the state of the simulation.
-            //
-            // Note that it is okay, and in fact preferred, for you to add helper methods.
-            // For example, you should probably write private helper methods for collision
-            // detection and adjustment, and for ending the simulation by setting the
-            // proper endMessage.
-            //
 
-            for (int i = 0; i < balls.length; i++){
-              balls[i].applyFriction(); // ball 0 moves toward goal
-              balls[i].moveToward((i == 0 ? goal.x : balls[0].x), (i == 0 ? goal.y : balls[0].y)); // all the other balls move toward ball 0
+            for (int i = 0; i < balls.length; i++) {
+                balls[i].applyFriction();
+                balls[i].moveToward((i == 0 ? goal.x : balls[0].x), (i == 0 ? goal.y : balls[0].y));
             }
             adjustIfCollisions();
             endSimulationIfNecessary();
@@ -120,67 +97,62 @@ public class RobotSoccerSimulation extends JPanel {
     }
 
     public void endSimulationIfNecessary() {
-      // figure this one out
-        if (balls[0].speed <= 0){
-          endMessage = "Oh no!";
-        } else if(balls[0].inside(goal)){
-          endMessage = "GOOOAAAL!!!";
+        if (balls[0].speed <= 0) {
+            endMessage = "Oh no!";
+        } else if(balls[0].inside(goal)) {
+            endMessage = "GOOOAAAL!!!";
         }
       }
 
 
     public void adjustIfCollisions() {
       for (var b1 : balls) {
-        for (var b2 : balls) {
-          if (b1 != b2) {
-            var dx = b2.x - b1.x;
-            var dy = b2.y - b1.y;
-            double distance = Math.hypot(dx, dy);
-            double overlap = b1.radius + b2.radius - distance;
-            if (overlap > 0){
-              double adjustX = (overlap / 2) * (dx / distance);
-              double adjustY = (overlap / 2) * (dy / distance);
-              b1.x -= adjustX;
-              b1.y -= adjustY;
-              b2.x += adjustX;
-              b2.y += adjustY;
-            }
-
-          }
+          for (var b2 : balls) {
+              if (b1 != b2) {
+                  var dx = b2.x - b1.x;
+                  var dy = b2.y - b1.y;
+                  double distance = Math.hypot(dx, dy);
+                  double overlap = b1.radius + b2.radius - distance;
+                if (overlap > 0){
+                    double adjustX = (overlap / 2) * (dx / distance);
+                    double adjustY = (overlap / 2) * (dy / distance);
+                    b1.x -= adjustX;
+                    b1.y -= adjustY;
+                    b2.x += adjustX;
+                    b2.y += adjustY;
+               }
+             }
         }
       }
     }
 
-    private static double constrain(double value, double low, double high){
-      // contrains the range
-      return Math.min(Math.max(low, value), high);
+    private static double constrain(double value, double low, double high) {
+        return Math.min(Math.max(low, value), high);
     }
 
     public static void main(String[] args) {
       try {
         if (args.length != 5){
-          throw new IllegalArgumentException("Exactly five arguments required");
+            throw new IllegalArgumentException("Exactly five arguments required");
         }
-
         PLAYER_RADIUS = Double.parseDouble(args[0]);
         ENEMY_RADIUS = Double.parseDouble(args[1]);
         PLAYER_SPEED = Double.parseDouble(args[2]);
         ENEMY_SPEED = Double.parseDouble(args[3]);
         FRICTION = Double.parseDouble(args[4]);
       } catch (NumberFormatException e) {
-        System.err.println("Arguments must all be doubles");
-        return;
+            System.err.println("Arguments must all be doubles");
+            return;
       } catch (IllegalArgumentException e) {
-        System.err.println(e.getMessage());
-        return;
+            System.err.println(e.getMessage());
+            return;
       }
 
-          balls = new Ball[] {
-          new Ball(0.0, HEIGHT, PLAYER_RADIUS, PLAYER_SPEED, Color.BLUE), // change x and y for visibility
-          new Ball(WIDTH * 0.25, 40, ENEMY_RADIUS, ENEMY_SPEED, Color.RED),
-          new Ball(WIDTH * 0.75, 40, ENEMY_RADIUS, ENEMY_SPEED, Color.RED),
-          new Ball(WIDTH / 2, HEIGHT / 2, ENEMY_RADIUS, ENEMY_SPEED, Color.RED)
-      };
+        balls = new Ball[] {
+            new Ball(0.0, HEIGHT, PLAYER_RADIUS, PLAYER_SPEED, Color.BLUE),
+            new Ball(WIDTH * 0.25, 40, ENEMY_RADIUS, ENEMY_SPEED, Color.RED),
+            new Ball(WIDTH * 0.75, 40, ENEMY_RADIUS, ENEMY_SPEED, Color.RED),
+            new Ball(WIDTH / 2, HEIGHT / 2, ENEMY_RADIUS, ENEMY_SPEED, Color.RED) };
 
         SwingUtilities.invokeLater(() -> {
             var panel = new RobotSoccerSimulation();
